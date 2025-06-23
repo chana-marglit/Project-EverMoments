d5f1004e467f5a3c5754d900a2709e2729116b36
chana-marglit <ch8457653@gmail.com>
2025-06-24 01:56:04 +0300

    Update IImageRepository.cs

--- EverMoments-Api/EverMoments-Core/Repositories/IImageRepository.cs
+++ EverMoments-Api/EverMoments-Core/Repositories/IImageRepository.cs
@@ -15,15 +15,12 @@ namespace EverMoments_Core.Repositories
         Task UpdateAsync(Image image);
         Task DeleteAsync(int id);
         Task<IEnumerable<Image>> SearchAsync(int albumId, string searchTerm);
-<<<<<<< HEAD
         Task<IEnumerable<Image>> SearchByTagAsync(string tag);
         Task<int> CountAsync();
         Task AddImageShareAsync(int imageId, int userId);
         Task<bool> IsImageSharedWithUser(int imageId, int userId);
         Task<List<Image>> GetImagesForUserAsync(int userId);
         Task SaveChangesAsync();
-=======
->>>>>>> fb84175d8ee8c740d20b5bbc67b3803ead778611
     }
 }
 
