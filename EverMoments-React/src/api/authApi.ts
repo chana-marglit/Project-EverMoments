c67dbd67b9cf5455415db09287e166263764dbc8
chana-marglit <ch8457653@gmail.com>
2025-06-24 00:04:55 +0300

    Update authApi.ts

--- EverMoments-React/src/api/authApi.ts
+++ EverMoments-React/src/api/authApi.ts
@@ -1,40 +1,4 @@
-<<<<<<< HEAD
-// import axios from 'axios';
 
-// // כתובת ה-API של צד השרת (עדכני לפי הפרויקט שלך)
-// const API_URL = import.meta.env.VITE_API_BASE + "/api";
-
-// export interface RegisterData {
-//   firstName: string;
-//   lastName: string;
-//   email: string;
-//   password: string;
-// }
-
-// export interface LoginData {
-//   email: string;
-//   password: string;
-// }
-
-// export const registerUser = async (userData: RegisterData): Promise<void> => {
-//   try {
-//     await axios.post(`${API_URL}/register`, userData);
-//   } catch (error) {
-//     console.error('שגיאה בהרשמה:', error);
-//     throw error;
-//   }
-// };
-
-// export const loginUser = async (credentials: LoginData): Promise<string> => {
-//   try {
-//     const response = await axios.post(`${API_URL}/login`, credentials);
-//     return response.data.token; // מחזיר את ה-JWT
-//   } catch (error) {
-//     console.error('שגיאה בהתחברות:', error);
-//     throw error;
-//   }
-// };
-=======
 import axios from 'axios';
 
 // כתובת ה-API של צד השרת (עדכני לפי הפרויקט שלך)
@@ -70,4 +34,4 @@ export const loginUser = async (credentials: LoginData): Promise<string> => {
     throw error;
   }
 };
->>>>>>> fb84175d8ee8c740d20b5bbc67b3803ead778611
+
