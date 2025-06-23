1a06634d7a15d87c9876be1485add6d1c6bf2eab
chana-marglit <ch8457653@gmail.com>
2025-06-24 02:06:17 +0300

    Update ImageDto.cs

--- EverMoments-Api/EverMoments-Core/DTO/ImageDto.cs
+++ EverMoments-Api/EverMoments-Core/DTO/ImageDto.cs
@@ -1,31 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+// ﻿using System;
+// using System.Collections.Generic;
+// using System.ComponentModel.DataAnnotations;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
 
-namespace EverMoments_Core.DTO
-{
-    public class ImageDto
-    {
-        [Required]
-        public string FileName { get; set; } = string.Empty;
+// namespace EverMoments_Core.DTO
+// {
+//     public class ImageDto
+//     {
+//         [Required]
+//         public string FileName { get; set; } = string.Empty;
 
-        [Required]
-        [MaxLength(20)]
-        public string FileType { get; set; } = string.Empty;
+//         [Required]
+//         [MaxLength(20)]
+//         public string FileType { get; set; } = string.Empty;
 
-        [Required]
-        public long FileSize { get; set; }
+//         [Required]
+//         public long FileSize { get; set; }
 
-        [Required]
-        public string S3Key { get; set; } = string.Empty;
+//         [Required]
+//         public string S3Key { get; set; } = string.Empty;
 
-        public int? AlbumId { get; set; } // אופציונלי – אם יש שיוך לאלבום
+//         public int? AlbumId { get; set; } // אופציונלי – אם יש שיוך לאלבום
 
-        public int? UserId { get; set; } // אופציונלי – אם את רוצה לשמור מזהה משתמש
+//         public int? UserId { get; set; } // אופציונלי – אם את רוצה לשמור מזהה משתמש
 
-        public string? Tags { get; set; } // תגיות אופציונליות (למשל: "טיול, משפחה")
-    }
-}
+//         public string? Tags { get; set; } // תגיות אופציונליות (למשל: "טיול, משפחה")
+//     }
+// }
