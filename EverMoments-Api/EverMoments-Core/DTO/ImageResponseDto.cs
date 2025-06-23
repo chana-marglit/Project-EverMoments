8b89c96d113be7fcebfdaee16fed3578e656f11e
chana-marglit <ch8457653@gmail.com>
2025-06-24 02:06:39 +0300

    Update ImageResponseDto.cs

--- EverMoments-Api/EverMoments-Core/DTO/ImageResponseDto.cs
+++ EverMoments-Api/EverMoments-Core/DTO/ImageResponseDto.cs
@@ -1,17 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
 
-namespace EverMoments_Core.DTO
-{
-    public class ImageResponseDto
-    {
-        public int Id { get; set; }
-        public string FileName { get; set; } = string.Empty;
-        public string FileType { get; set; } = string.Empty;
-        public string FileUrl { get; set; } = string.Empty;
-        public DateTime UploadedAt { get; set; }
-    }
-}
+// namespace EverMoments_Core.DTO
+// {
+//     public class ImageResponseDto
+//     {
+//         public int Id { get; set; }
+//         public string FileName { get; set; } = string.Empty;
+//         public string FileType { get; set; } = string.Empty;
+//         public string FileUrl { get; set; } = string.Empty;
+//         public DateTime UploadedAt { get; set; }
+//     }
+// }
