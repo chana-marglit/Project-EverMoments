ab1cd2a3b4927034b44ab0f95e0f9875adcc7978
chana-marglit <ch8457653@gmail.com>
2025-06-24 01:54:24 +0300

    Update ILogService.cs

--- EverMoments-Api/EverMoments-Core/Interfaces/ILogService.cs
+++ EverMoments-Api/EverMoments-Core/Interfaces/ILogService.cs
@@ -11,9 +11,6 @@ namespace EverMoments_Core.Interfaces
     {
         Task<IEnumerable<Log>> GetLogsByUserIdAsync(int userId);
         Task AddLogAsync(int userId, string action, string? description = null);
-<<<<<<< HEAD
         Task<int> CountLogsAsync();
-=======
->>>>>>> fb84175d8ee8c740d20b5bbc67b3803ead778611
     }
 }
