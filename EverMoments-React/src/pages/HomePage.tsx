79fb287b529ca5126ebc7a4bf1e35db87299ec00
chana-marglit <ch8457653@gmail.com>
2025-07-21 17:42:17 +0300

    Update HomePage.tsx

--- EverMoments-React/src/pages/HomePage.tsx
+++ EverMoments-React/src/pages/HomePage.tsx
@@ -98,7 +98,7 @@ const HomePage: React.FC = () => {
           flex-direction: column;
           justify-content: center;
           align-items: center;
-          backgroundImage: `url(${backgroundImage})`,
+          background-image: url('${backgroundImage}');
           background-size: cover;
           background-position: center;
           color: white;
